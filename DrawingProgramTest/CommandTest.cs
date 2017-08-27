117f2322d0a3718e49245871c582a032579e667c
ckwongad <ckwongad@gmail.com>
2017-08-27 17:48:21 +0800

    - added moq package - used moq for behavior testing on commands

--- DrawingProgramTest/CommandTest.cs
+++ DrawingProgramTest/CommandTest.cs
@@ -1,16 +1,73 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using DrawingProgram.Canvas;
+using DrawingProgram.Command;
+using DrawingProgram.Command.CommandArgs;
 
 namespace DrawingProgramTest
 {
     [TestClass]
     public class CommandTest
     {
-        //TODO: use Moq to do behavior test for Commands
+        [TestMethod]
+        public void CanvasCommand_ExcuteShouldCallCreateCanvas()
+        {
+            var mock = new Mock<ICanvas>();
+
+            var createCanvasArgs = new int[] { 5, 5 };
+            mock.Setup(_canvas => _canvas.CreateCanvas(createCanvasArgs));
+
+            var canvas = mock.Object;
+            var canvasCommand = new CanvasCommand(canvas, createCanvasArgs);
+            canvasCommand.Execute();
+
+            mock.Verify(_canvas => _canvas.CreateCanvas(createCanvasArgs), Times.Once);
+        }
 
         [TestMethod]
-        public void TestMethod1()
+        public void LineCommand_ExcuteShouldCallDrawLine()
         {
+            var mock = new Mock<ICanvas>();
+
+            var drawLineArgs = new int[] { 5, 5, 5, 7 };
+            mock.Setup(_canvas => _canvas.DrawLine(drawLineArgs));
+
+            var canvas = mock.Object;
+            var canvasCommand = new LineCommand(canvas, drawLineArgs);
+            canvasCommand.Execute();
+
+            mock.Verify(_canvas => _canvas.DrawLine(drawLineArgs), Times.Once);
+        }
+
+        [TestMethod]
+        public void RectangleCommand_ExcuteShouldCallDrawRectrangle()
+        {
+            var mock = new Mock<ICanvas>();
+
+            var drawRectangleArgs = new int[] { 5, 5, 8, 9 };
+            mock.Setup(_canvas => _canvas.DrawRectangle(drawRectangleArgs));
+
+            var canvas = mock.Object;
+            var canvasCommand = new RectangleCommand(canvas, drawRectangleArgs);
+            canvasCommand.Execute();
+
+            mock.Verify(_canvas => _canvas.DrawRectangle(drawRectangleArgs), Times.Once);
+        }
+
+        [TestMethod]
+        public void FillCommand_ExcuteShouldCallFill()
+        {
+            var mock = new Mock<ICanvas>();
+
+            var fillArgs = new FillCommandArgs(5, 5, 'c');
+            mock.Setup(_canvas => _canvas.Fill(fillArgs));
+
+            var canvas = mock.Object;
+            var canvasCommand = new FillCommand(canvas, fillArgs);
+            canvasCommand.Execute();
+
+            mock.Verify(_canvas => _canvas.Fill(fillArgs), Times.Once);
         }
     }
 }
